053bfefa1a9b1e89e0a161f1b872358b954a0b6d
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-07-31 21:41:25 +0200

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  var i1: string = 'jézuska';
-  var i2: string = 'géppuska';
-  import levenshtein from 'damerau-levenshtein';
-  $: sim = levenshtein(i1, i2).similarity.toFixed(2);
+	var i1: string = 'jézuska';
+	var i2: string = 'géppuska';
+	import levenshtein from 'damerau-levenshtein';
+	$: sim = levenshtein(i1, i2).similarity.toFixed(2);
 </script>
 
 <h1>Két szöveg hasonlósága</h1>
@@ -10,4 +10,11 @@
 <br />
 <input type="text" bind:value={i2} />
 <br />
-Hasonlóság: {sim}
+<div>Hasonlóság: {sim}</div>
+
+<style lang="scss">
+  $resc: rgb(55, 71, 109);
+	div {
+    color: $resc;
+	}
+</style>
