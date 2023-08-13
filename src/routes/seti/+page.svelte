01ed2447a378353ddade410e907f668a2df7b70f
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-08-13 21:11:53 +0200

    .

---
+++ src/routes/seti/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  var i = 0
+  setInterval( ()=> i++, 1000)
+</script>
+<body>
+  <h1>Négyzetszámok</h1>
+  {i}<sup>2</sup> = {i ** 2}
+</body>
+<style>
+  * {
+    text-align: center;
+    background: saddlebrown;
+    color: wheat;
+  }
+</style>
