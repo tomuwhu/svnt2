337b1d08a4e2084dc0be22b79c19b230be132a4e
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-08-13 21:12:49 +0200

    .

--- src/routes/seti/+page.svelte
+++ src/routes/seti/+page.svelte
@@ -1,12 +1,12 @@
-<script>
-  var i = 0
+<script lang="ts">
+  var i: number = 0
   setInterval( ()=> i++, 1000)
 </script>
 <body>
   <h1>Négyzetszámok</h1>
   {i}<sup>2</sup> = {i ** 2}
 </body>
-<style>
+<style lang="scss">
   * {
     text-align: center;
     background: saddlebrown;
